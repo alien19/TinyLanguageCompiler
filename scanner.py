4e422f06c23e74f77c90f8dc04f86dc45d81658b
theMeanScore <m.ad.ali2165@gmail.com>
2020-12-11 04:50:05 +0200

    Finish the project

--- scanner.py
+++ scanner.py
@@ -7,24 +7,29 @@ class TokenType(Enum):
   ELSE = auto()
   WRITE = auto()
   READ = auto()
-  BEGIN = auto()
+  UNTIL = auto()
+  # BEGIN = auto()
   END = auto()
-  MAIN = auto()
-  STRING = auto()
-  REAL = auto()
-  INT = auto()
-  RETURN = auto()
+  # MAIN = auto()
+  # STRING = auto()
+  # REAL = auto()
+  # INT = auto()
+  # RETURN = auto()
+  REPEAT = auto()
+
   SEMICOLON = auto()
   OPEN_PARAN = auto()
   CLOSE_PARAN = auto()
-  COMMA = auto()
+  LESS_THAN = auto()
+  # COMMA = auto()
   PLUS = auto()
   MINUS = auto()
   MULT = auto()
   DIVIDE = auto()
   ASSIGN = auto()
   EQUAL = auto()
-  NOT_EQUAL = auto()
+  # NOT_EQUAL = auto()
+
   NUM = auto()
   ID = auto()
 
@@ -34,24 +39,28 @@ MAP_PREDEFINED_TO_ENUM = {
   'else': TokenType.ELSE,
   'write': TokenType.WRITE,
   'read': TokenType.READ,
-  'begin': TokenType.BEGIN,
+  # 'begin': TokenType.BEGIN,
   'end': TokenType.END,
-  'main': TokenType.MAIN,
-  'string': TokenType.STRING,
-  'real': TokenType.REAL,
-  'int': TokenType.INT,
-  'return': TokenType.RETURN,
+  'repeat': TokenType.REPEAT,
+  'until': TokenType.UNTIL,
+  # 'main': TokenType.MAIN,
+  # 'string': TokenType.STRING,
+  # 'real': TokenType.REAL,
+  # 'int': TokenType.INT,
+  # 'return': TokenType.RETURN,
   ';': TokenType.SEMICOLON,
   '(': TokenType.OPEN_PARAN,
   ')': TokenType.CLOSE_PARAN,
-  ',': TokenType.COMMA,
+  '<': TokenType.LESS_THAN,
+  # ',': TokenType.COMMA,
   '+': TokenType.PLUS,
   '-': TokenType.MINUS,
   '*': TokenType.MULT,
   '/': TokenType.DIVIDE,
   ':=': TokenType.ASSIGN,
-  '==': TokenType.EQUAL,
-  '!=': TokenType.NOT_EQUAL,
+  '=': TokenType.EQUAL,
+  # '==': TokenType.EQUAL,
+  # '!=': TokenType.NOT_EQUAL,
 }
 
 MAP_FLEXIBLE_TO_ENUM = {
@@ -60,18 +69,20 @@ MAP_FLEXIBLE_TO_ENUM = {
   '[_a-zA-Z]([_a-zA-Z0-9])*': TokenType.ID
 }
 
-FILE_PATH = "test.tiny"
-OPERATORS = ["*", "+", "-", "/", ":=", "==", "!=", "(", ")", ",", ";"]
+FILE_PATH = "example1.tiny"
+OPERATORS = ["*", "+", "-", "/", ":=", "(", ")", ",", ";"]
 
 f = open(FILE_PATH, "r")
 content = "".join(f.readlines())
-content = re.sub('/\*\*(.|\n)*\*\*/', '', content)
+content = re.sub('\{(.|\n)*?\}', '', content)
 lines = content.split("\n")
 strings = []
 
 for line in lines:
     for op in OPERATORS:
         line = line.replace(op, " "+op+" ")
+    line = line.replace("=", " = ")
+    line = line.replace(": =", ":=")
     line = line.replace("  ", " ")
     strings += line.split(" ")
 
@@ -85,13 +96,26 @@ for string in strings:
   if string.lower() in MAP_PREDEFINED_TO_ENUM.keys():
     stringsAndTokens.append((string, MAP_PREDEFINED_TO_ENUM[string.lower()]))
     continue
+  flag = True
   for regex in MAP_FLEXIBLE_TO_ENUM.keys():
-    print(regex)
     if bool(re.match(regex, string)):
       stringsAndTokens.append((string, MAP_FLEXIBLE_TO_ENUM[regex]))
+      flag = False
       continue
-  print(string)
-  raise Exception("Unknown Token")
+  if flag:
+    print(string)
+    raise Exception("Unknown Token")
+
+result = ""
 
 for stringAndToken in stringsAndTokens:
-  print(stringAndToken[0], stringAndToken[1])
+  result += ("{}\t{}\n".format(stringAndToken[0], stringAndToken[1].name))
+
+result = result[:-1]
+f = open("result.txt", "w")
+f.write(result)
+f.close()
+
+print(result)
+
+
