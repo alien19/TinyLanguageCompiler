fbb919e98beec32efa3478bf37d5ddeb5812d8ba
alien19 <1601374@eng.asu.edu.eg>
2020-12-24 03:07:16 +0200

    Implement parse_tree

--- node.py
+++ node.py
@@ -1,16 +1,24 @@
+<<<<<<< Updated upstream
 from common import TokenType, StructType
 
 # TODO
+||||||| constructed merge base
+# TODO
+=======
+# from common import StructType, TokenType
+
+
+>>>>>>> Stashed changes
 class Node:
-    def __init__(self, text:str, value: str, structVal: StructType, tokenType: TokenType):
-      self.text:str = text
-      self.value:str = value
-      self.structVal:StructType = structVal
-      self.tokenType:TokenType = tokenType
-      self.nxt:Node = None
-      self.prev:Node = None
-      self.parent:Node = None
-      self.children:list = []
-    
-    def add_child(node:Node):
-      pass
+    def __init__(self, text: str, value: str, structVal, tokenType):
+        self.text: str = text
+        self.value: str = value
+        self.structVal = structVal
+        self.tokenType = tokenType
+        self.nxt: Node = None
+        self.prev: Node = None
+        self.parent: Node = None
+        self.children: list = []   
+
+    def add_child(self, node):
+        self.children.append(node)
