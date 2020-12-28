84a11411c21d17102eee40b851a3506f97a12d5b
Mohamed Gamal <mgtmprog@gmail.com>
2020-12-28 05:40:42 +0200

    changes again from me2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from common import StructType
 from compiler import Compiler
-from graphviz import Digraph
+from graphviz import Source
 
 f = open("example1.tiny", "r")
 content = "".join(f.readlines())
@@ -9,13 +9,10 @@ f.close()
 c = Compiler(text=content)
 it = c.get_parse_tree_iterator()
 
-# f = Digraph('finite_state_machine', filename='fsm.png')
-# f.attr(rankdir='LR', size='8,5')
-
-# f.attr('node', shape='circle')
-
 nodes = {}
 sameRank = {}
+squares = []
+elipses = []
 links = []
 
 def iterate(it, index, previous=-1):
@@ -23,8 +20,10 @@ def iterate(it, index, previous=-1):
     if index not in sameRank: sameRank[index] = []
     sameRank[index].append(it.get_node_id())
     nodes[it.get_node_id()] = it
-    if previous != -1: links.append((nodes[previous].get_text(), it.get_text()))
-    
+    if previous != -1: links.append((nodes[previous].get_node_id(), it.get_node_id()))
+    if it.get_struct_type() == StructType.EXPRESSION: elipses.append(it.get_node_id())
+    else: squares.append(it.get_node_id())
+
     for t in it.children():
         iterate(t, index+1, it.get_node_id())
     if it.has_next():
@@ -32,5 +31,35 @@ def iterate(it, index, previous=-1):
 
 iterate(it, 0)
 
-print(sameRank)
-print(links)
+graph = """
+graph {
+    {
+SHAPE_GRAPH
+    }
+SHAPE_LINK
+RANK_GRAPH
+}
+"""
+
+shapes_graph = ""
+for id in squares:
+    shapes_graph += "\t{} [shape=\"square\" label=\"{}\"]\n".format(id, nodes[id].get_text())
+for id in elipses:
+    shapes_graph += "\t{} [shape=\"ellipse\" label=\"{}\"]\n".format(id, nodes[id].get_text())
+links_graph = ""
+for link in links:
+    links_graph += "{} -- {};\n".format(link[0], link[1])
+rank_graph = ""
+for index in sameRank:
+    rank_graph += "{rank=same;"
+    for i in sameRank[index]:
+        rank_graph += i + " ,"
+    rank_graph = rank_graph[:-2]
+    rank_graph += "}\n"
+
+graph = graph.replace("SHAPE_GRAPH", shapes_graph)
+graph = graph.replace("SHAPE_LINK", links_graph)
+graph = graph.replace("RANK_GRAPH", rank_graph)
+
+s = Source(graph, filename="test.gv", format="png")
+s.view()
--- testing_graph.py
+++ testing_graph.py
@@ -2,12 +2,56 @@ from graphviz import Source
 temp = """
 graph {
     {
-        0 [shape="square" label="Ali"]
+        0 [shape="square" label="read"]
+        20 [shape="square" label="if"]
+        5 [shape="square" label="assign"]
+        17 [shape="square" label="repeat"]
+        9 [shape="square" label="assign"]
+        13 [shape="square" label="assign"]
+        19 [shape="square" label="write"]
+        2 [shape="ellipse" label="op"]
+        1 [shape="ellipse" label="const"]
+        3 [shape="ellipse" label="id"]
+        4 [shape="ellipse" label="const"]
+        7 [shape="ellipse" label="op"]
+        6 [shape="ellipse" label="id"]
+        8 [shape="ellipse" label="id"]
+        11 [shape="ellipse" label="op"]
+        10 [shape="ellipse" label="id"]
+        12 [shape="ellipse" label="const"]
+        15 [shape="ellipse" label="op"]
+        14 [shape="ellipse" label="id"]
+        16 [shape="ellipse" label="const"]
+        18 [shape="ellipse" label="id"]
+
     }
-0 -- b;
-b -- c;
-b -- d;
-{ rank=same; b, 0 }
+0 -- 20;
+20 -- 2;
+2 -- 1;
+2 -- 3;
+20 -- 5;
+5 -- 4;
+5 -- 17;
+17 -- 9;
+9 -- 7;
+7 -- 6;
+7 -- 8;
+9 -- 13;
+13 -- 11;
+11 -- 10;
+11 -- 12;
+17 -- 15;
+15 -- 14;
+15 -- 16;
+17 -- 19;
+19 -- 18;
+
+{rank=same;0 ,20}
+{rank=same;2 ,5 ,17 ,19}
+{rank=same;1 ,3 ,4 ,9 ,13 ,15 ,18}
+{rank=same;7 ,11 ,14 ,16}
+{rank=same;6 ,8 ,10 ,12}
+
 }
 """
 
